11221f40a86faa1cd3d13b79b0945538b1153e94
cypekSchool <mojcyprian9@gmail.com>
2024-09-12 11:06:01 +0200

    upload Files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+
+        int[] randomNumbers = new int[6];
+
+        for (int i = 0; i < randomNumbers.length; i++) {
+            randomNumbers[i] = (int)(Math.random() * 100);
+        }
+
+        for (int number : randomNumbers)
+        {
+            System.out.print(number + ", ");
+        }
+
+
+        ArrayList<Integer> randomNumbersList = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            randomNumbersList.add( (int)(Math.random() * 100) );
+        }
+
+        System.out.println(randomNumbersList);
     }
 }
